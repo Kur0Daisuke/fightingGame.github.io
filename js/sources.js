e41c343c62c47dc07f7d06efd0d39306c7e0c6de
Kur0Daisuke <90145022+Kur0Daisuke@users.noreply.github.com>
2023-08-23 16:03:08 +0630

    Update sources.js

--- js/sources.js
+++ js/sources.js
@@ -6,7 +6,7 @@ const images = [
     "./img/kenji/Idle.png",
     "./img/kenji/Jump.png",
     "./img/kenji/Run.png",
-    "./img/kenji/Take Hit.png",
+    "./img/kenji/TakeHit.png",
     
     "./img/samuraiMack/Attack1.png",
     "./img/samuraiMack/Attack2.png",
@@ -15,7 +15,7 @@ const images = [
     "./img/samuraiMack/Idle.png",
     "./img/samuraiMack/Jump.png",
     "./img/samuraiMack/Run.png",
-    "./img/samuraiMack/Take Hit.png",
+    "./img/samuraiMack/TakeHit.png",
 
     "./img/background.png",
     "./img/wood.png"
